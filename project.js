479c25347a3a9fddb6aa53fb001111cf61d90eeb
Tarsha Franklin <tarshafranklin@andrews-mbp.lan>
2020-10-12 08:18:31 -0600

    submit changes

--- project.js
+++ project.js
@@ -16,42 +16,20 @@
 //2. Build on the Q&A by adding more Q&A's
 //3. Give the app some interactive properties like voice
 //("Good Job!")
-activityTitle.style.textAlign = 'center';
-activityTitle.style.fontWeight = 'bold';
-const subTitle = document.querySelector('');
-aside.appendChild(subTitle);
-subTitle.style.fontSize = '25px';
-subTitle.style.fontWeight = 'bold';
-
-const jaaFunc = document.createElement('h2');
-tableOfContentsTitle.innerText = 'Let\'s Learn JavaScript';
-const aside = document.querySelector('aside');
-console.log(aside);
-aside.appendChild(jaaFunc);
-
-let questBank = [
-	{ image: 'arr1.png', answer: 'The slice method.' },
-	{ image: 'arr2.png', answer: 'The pop method.' },
-	{ image: 'arr3.png', answer: 'The Value.' },
-	{ image: 'arr4.png', answer: 'For Loop' },
-	{ image: 'func5.png', answer: 'Data-Types' },
-	{ image: 'func4.png', answer: 'A nested array.' },
-	{ image: 'func3.png', answer: 'A Keyword.' },
-	{ image: 'func2.png', answer: 'The function declaration syntax.' },
-	{ image: 'func1.png', answer: 'The arrow method.' },
-	{ image: 'funcArr.png', answer: 'The anonyamous function.' },
-];
-for (let i = 0; i < arr.length; i += 1) {
-	let items = getRandomItem(questBank);
-	arr[i].innerHTML = arr.image + '<img src="' + arr.answer + '">';
-	//console.log(questBank)
-}
-const answerDisplay = document.getElementById('answer').style.display = 'none';
-    let ans = document.getElementById('answer');
-    if(ans.style.display === 'none') {
-        ans.style.display = 'block';
-    
-    }else {
-        ans.style.display = 'none';
-    }
-}
+const activityTitle = document.getElementById('activityTitle');
+const title = document.getElementById('title');
+console.log('title');
+const areaButton = document.querySelector('#buttonArea');
+const cardArea = document.getElementsByClassName('cardArea');
+// let questBank = [
+// 	{ image: '', answer: 'Anonymous function.' },
+// 	{ image: 'https://imgur.com/omn4g13: 'The for loop.' },
+// 	{ image: 'https://imgur.com/xSEa5Fg', answer: 'The Value.' },
+// 	{ image: 'https://imgur.com/9PWUJU4', answer: 'the pop method' },
+// 	{ image: 'https://imgur.com/XlVAm0f', answer: 'the slice method' },
+// 	{ image: 'https://imgur.com/2TGc3NY', answer: 'A nested array.' },
+// 	{ image: 'https://imgur.com/ZMsBrlR', answer: 'A nested array.' },
+// 	{ image: 'https://imgur.com/MtV1Hqq', answer: 'The keyword.' },
+// 	{ image: 'https://imgur.com/R7GTbyC', answer: 'function declaration syntax.' },
+// 	{ image: 'https://imgur.com/uRtu2kq', answer: 'True' },
+// ]
