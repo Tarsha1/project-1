c3bb64e2f94618cc4bfd86e2bc5aec6d8186ab76
Tarsha Franklin <tarshafranklin@TashaMakbookAir.attlocal.net>
2020-10-18 21:01:09 -0500

    Added a Score

--- project.js
+++ project.js
@@ -1,3 +1,5 @@
+var score = 0;
+
 const openBtn = document.getElementById('openModal');
 const modal = document.getElementById('modal');
 const close = document.getElementById('close');
@@ -47,89 +49,57 @@ let questBank = [
 		correctAnswer: 'a',
 	},
 ];
-
+const playersScore = document.getElementById('score');
+playersScore.innerText = 'score:' + score;
 const btn = document.getElementById('btn');
-
 //initialize an index variable to store current array element index
-let index = 0;
+var index = 0;
 const questionArea = document.querySelector('.cardArea');
 //setting the question innerText to the first array element
 questionArea.innerText = questBank[index].questVal;
 const answerArea = document.getElementsByClassName('answerArea')[0];
+console.log('ANSWER AREA: ', answerArea);
+const optionA = document.getElementById('optionA');
+const optionB = document.getElementById('optionB');
 const buttonArea = document.querySelector('.buttonArea');
 btn.addEventListener('click', () => {
-	answerArea.innerText =
-		questBank[index].answers[0] + ' ' + questBank[index].answers[1];
-
+	optionA.innerText = questBank[index].answers[0];
+	optionB.innerText = questBank[index].answers[1];
+	// answerArea.innerText = questBank[index].answers[0] + ' ' + questBank[index].answers[1];
 	console.log(index);
+});
+optionA.addEventListener('click', () => {
+	console.log('OPTION A CLICKED');
+	// console.log(optionA.innerText[0])
+	if (optionA.innerText[0] === questBank[index].correctAnswer) {
+		console.log('CORRECT');
+		score += 10;
+		index += 1;
+		playersScore.innerText = 'score:' + score;
+	} else {
+		console.log('WRONG');
+		alert('wrong answer');
+		index += 1;
+	}
+});
+optionB.addEventListener('click', () => {
+	console.log('OPTION B CLICKED');
+	if (optionB.innerText[0] === questBank[index].correctAnswer) {
+		console.log('CORRECT');
+		alert('Correct');
+		score += 10;
+		index += 1;
+		playersScore.innerText = 'score:' + score;
+	} else {
+		console.log('WRONG');
+		alert('Wrong Answer');
+		index += 1;
+	}
 });
 
 let nextButton = document.getElementsByClassName('nextButton')[0];
 nextButton.addEventListener('click', () => {
-	index++;
-	if (5 <= index) {
-		alert('Well Done!');
-		index = 0;
-		questionArea.innerText = questBank[index].questVal;
-	} else {
-		questionArea.innerText = questBank[index].questVal;
-		answerArea.innerText = '';
-	}
-	console.log(questBank.length);
-	console.log(index);
+	questionArea.innerText = questBank[index].questVal;
+	optionA.innerText = questBank[index].answers[0];
+	optionB.innerText = questBank[index].answers[1];
 });
-
-//reset button and text
-questionArea.addEventListener('reset', () => {
-	questionArea.innerText = question[index].answer;
-	buttonArea.innerText = 'Well Done!';
-});
-//Score board
-function finalScore() {
-	let i = 0;
-	let correct = 10;
-	let selectValue;
-	let question = document.getElementsByClassName('questBank');
-	let select = document.getElementById('questVal');
-	let answer = select.options[select.selectedIndex].value;
-	if (answer == 'right') {
-		i++;
-	}
-
-	let select2 = document.getElementById('questVal2');
-	let answer2 = select2.options[select2.selectedIndex].value;
-	if (answer2 == 'right') {
-		i++;
-	}
-
-	let select3 = document.getElementById('questVal3');
-	let answer3 = select3.options[select3.selectedIndex].value;
-	if (answer3 == 'right') {
-		i++;
-	}
-	let select4 = document.getElementById('questVal4');
-	let answer4 = select4.options[select4.selectedIndex].value;
-	if (answer4 == 'right') {
-		i++;
-	}
-	let select5 = document.getElementById('questVal5');
-	let answer5 = select5.options[select5.selectedIndex].value;
-	if (answer5 == 'right') {
-		i++;
-	}
-	document.getElementById('scoreDisplay').innerHTML = index;
-}
-function finalScore() {
-	let answersCorrect = 0;
-	let answersCounter = 0;
-	foreach('select');
-	{
-		let rightOrWrong = 'get value';
-		if ((rightOrWrong = right)) {
-			answersCorrect++;
-		}
-		answersCounter++;
-	}
-	let finalScore = Math.floor((answersCorrect / answersCounter) * 20);
-	document.getElementById('scoreDisplay').innerHTML = finalScore;
-}
